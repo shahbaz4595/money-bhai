f6c391e1d2b4e1e0e92d74168fdb2350f97b42a2
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2023-03-30 06:15:44 +0530

    fix: fixed login routing issue

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -7,28 +7,32 @@ const Auth = ({ type = 'login' }) => {
   const { signup, isLoading, error } = useSignup()
 
   return (
-    <section className={style['auth-page']}>
-      <div className={style.tagline}>
-        <Tagline />
-      </div>
-      <div className={style['auth-form']}>
-        {isLoading && type === 'login' ? (
-          <LoginForm
-            onSubmit={(data) => {
-              console.log(data)
-            }}
-          />
-        ) : (
-          <SignupForm
-            onSubmit={({ email, password, firstName, lastName }) =>
-              signup(email, password, firstName, lastName)
-            }
-          />
+    <>
+      <section className={style['auth-page']}>
+        <div className={style.tagline}>
+          <Tagline />
+        </div>
+        {!isLoading && (
+          <div className={style['auth-form']}>
+            {type === 'login' ? (
+              <LoginForm
+                onSubmit={(data) => {
+                  console.log(data)
+                }}
+              />
+            ) : (
+              <SignupForm
+                onSubmit={({ email, password, firstName, lastName }) =>
+                  signup(email, password, firstName, lastName)
+                }
+              />
+            )}
+          </div>
         )}
-      </div>
+      </section>
       {error && <p>{error}</p>}
       {isLoading && <p>Loading...</p>}
-    </section>
+    </>
   )
 }
 
